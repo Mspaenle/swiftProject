7dc24d6d9b5b8996077ed7368b5aed9d0a9b6ce3
Chloe DALGER <chloe.dalger@igtp3-mac10.isim.intra>
2018-03-05 18:54:05 +0100

    models

---
+++ PKProject/PKProject/ActivityModel.swift
@@ -0,0 +1,9 @@
+//
+//  ActivityModel.swift
+//  PKProject
+//
+//  Created by Chloe DALGER on 05/03/2018.
+//  Copyright © 2018 Mahe SPAENLE Chloé DALGER. All rights reserved.
+//
+
+import Foundation
---
+++ PKProject/PKProject/ListActivityModel.swift
@@ -0,0 +1,9 @@
+//
+//  ListActivityModel.swift
+//  PKProject
+//
+//  Created by Chloe DALGER on 05/03/2018.
+//  Copyright © 2018 Mahe SPAENLE Chloé DALGER. All rights reserved.
+//
+
+import Foundation
