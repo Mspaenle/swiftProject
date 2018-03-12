31a3d6aa0d03ccddfa4749246e6de06bfd5d250d
Mahe SPAENLE <mahe.spaenle@igtp3-mac11.isim.intra>
2018-03-12 16:31:52 +0100

    add

--- PKProject/PKProject/Controler/StateTableViewCell.swift
+++ PKProject/PKProject/Controler/StateTableViewCell.swift
@@ -2,7 +2,7 @@
 //  StateTableViewCell.swift
 //  PKProject
 //
-//  Created by Mahe SPAENLE on 10/03/2018.
+//  Created by Mahe SPAENLE on 12/03/2018.
 //  Copyright © 2018 Mahe SPAENLE Chloé DALGER. All rights reserved.
 //
 
@@ -10,18 +10,18 @@ import Foundation
 import UIKit
 
 class StateTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var StateDateLabel: UILabel!
     @IBOutlet weak var StateTitleLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
