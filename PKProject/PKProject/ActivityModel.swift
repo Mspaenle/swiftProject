21052b7ad43a8cddc1510adacb65cc2146d1ab48
Mahe SPAENLE <mahe.spaenle@igtp4-mac16.isim.intra>
2018-03-14 12:18:11 +0100

    Changements modèles

--- PKProject/PKProject/ActivityModel.swift
+++ PKProject/PKProject/ActivityModel.swift
@@ -10,6 +10,61 @@ import Foundation
 import CoreData
 import UIKit
 
-extension Activity{
+class ActivityModel{
+    private var dao : Activity
     
+    var date: NSDate {
+        get{
+            return self.dao.date!
+        }
+        set{
+            self.dao.date = newValue
+        }
+    }
+    
+    var title: String {
+        get{
+            return self.dao.title!
+        }
+        set{
+            self.dao.title = newValue
+        }
+    }
+    
+    var specification: String {
+        get{
+            return self.dao.specification!
+        }
+        set{
+            self.dao.specification = newValue
+        }
+    }
+    
+    var duration: Int16 {
+        get{
+            return self.dao.duration
+        }
+        set{
+            self.dao.duration = newValue
+        }
+    }
+    
+    var periodicity: Periodicity{
+        get{
+            return self.dao.periodicity!
+        }
+        set{
+            self.dao.periodicity = newValue
+        }
+    }
+    
+    
+    init(date : NSDate, title : String, specification : String, duration : Int16, periodicity: Periodicity){
+        let entity = CoreDataManager.entity(forName: "Activity")
+        self.dao = Activity(entity: entity, insertInto: CoreDataManager.context)
+        self.dao.date=date
+        self.dao.title=title
+        self.dao.specification=specification
+        
+    }
 }
