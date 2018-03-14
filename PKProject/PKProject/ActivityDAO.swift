bb9293bf7efa76b338ae70f7b24e3faa01b1c17d
Unknown <Mahe.spaenle@etu.umontpellier.fr>
2018-03-14 12:45:08 +0100

    DAO Activity

---
+++ PKProject/PKProject/ActivityDAO.swift
@@ -0,0 +1,20 @@
+//
+//  ActivityDAO.swift
+//  PKProject
+//
+//  Created by Mahe SPAENLE on 14/03/2018.
+//  Copyright © 2018 Mahe SPAENLE Chloé DALGER. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+extension Activity{
+    static func getNewStateDAO() -> Activity?{
+        guard let entity = NSEntityDescription.entity(forEntityName: "Activity", in: CoreDataManager.context) else {
+            return nil
+        }
+        return Activity(entity: entity, insertInto : CoreDataManager.context)
+    }
+}
---
+++ PKProject/PKProject/DosesDAO.swift
@@ -0,0 +1,20 @@
+//
+//  DosesDAO.swift
+//  PKProject
+//
+//  Created by Mahe SPAENLE on 14/03/2018.
+//  Copyright © 2018 Mahe SPAENLE Chloé DALGER. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+extension Dose{
+    static func getNewStateDAO() -> Dose?{
+        guard let entity = NSEntityDescription.entity(forEntityName: "Dose", in: CoreDataManager.context) else {
+            return nil
+        }
+        return Dose(entity: entity, insertInto : CoreDataManager.context)
+    }
+}
