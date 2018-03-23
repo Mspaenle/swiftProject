f9f849b4a9672dc78eecfc07315271e788fba714
Mahe SPAENLE <mahe.spaenle@igtp3-mac05.isim.intra>
2018-03-23 15:02:32 +0100

    edit add drug intakes

--- PKProject/PKProject/Controler/AddPillsViewController.swift
+++ PKProject/PKProject/Controler/AddPillsViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class AddPillsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class AddPillsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var MedPicker: UIPickerView!
+    
+    
     @IBOutlet weak var DateTable: UITableView!
-    var dates: [Date] = []
+    
     @IBOutlet weak var ValidateBTN: UIButton!
     @IBOutlet weak var CancelBTN: UIButton!
     
+    var drugIntake: DrugIntakeModel?
+    var med: MedModel?
+    var dates: [Date] = []
+    var dose: String?
+    
     @IBAction func listDatesAction(_ sender: Any) {
-        
+
     }
     
+    @IBAction func validateAction(_ sender: Any) {
+        if sender as! UIButton == self.ValidateBTN {
+            drugIntake = DrugIntakeModel(med: med!, periodicity: dates, dose: dose!)
+        }
+        else {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +56,40 @@ class AddPillsViewController: UIViewController, UITableViewDelegate, UITableView
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return self.dates.count
+    }
+    
+    func tableView(_ tableView: UITableView,canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableView, indexPath: IndexPath){
+        if (editingStyle==UITableViewCellEditingStyle.delete){
+            self.DateTable.beginUpdates()
+            if self.delete(dateWithIndex: indexPath.row){
+                self.dateWithIndex.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+            }
+            self.DateTable.endUpdates()
+        }
+    }
+    
+    func delete(dateWithIndex index: Int) -> Bool{
+        guard let context = self.getContext(errorMsg: "Ne peut pas supprimer de date") else {return false}
+        let date = self.dates[index]
+        context.delete(date)
+        do{
+            try context.save()
+            self.dates.remove(at: index)
+            return true
+        }
+    }
+    
+    @IBAction func unwindToAddPills(sender: UIStoryboardSegue){
+        if let controller = sender.source as? PopUpPillsDateViewController{
+            if let dateToSave = controller.heure{
+                self.dates.append(dateToSave)
+                self.DateTable.reloadData()
+            }
+        }
     }
         
     /*
